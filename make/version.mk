52fd6dd7543c59895e23cc0429351ace4ffe7b43
Brian D. Holland <5244318+brianholland@users.noreply.github.com>
2026-03-10 18:25:45 -0400

    fixed versioning to work on macos

--- make/version.mk
+++ make/version.mk
@@ -1,6 +1,9 @@
 # Shared version management targets
 # Requires VERSION to be defined in the including Makefile
 
+# Portable sed in-place: macOS requires -i '', GNU requires -i
+SED_I := $(shell if sed --version 2>/dev/null | grep -q GNU; then echo 'sed -i'; else echo 'sed -i ""'; fi)
+
 .PHONY: tag bump-patch bump-minor bump-major
 
 tag: ## create git tag for current VERSION
@@ -9,9 +12,9 @@ tag: ## create git tag for current VERSION
 
 bump-patch: ## bump patch version (x.y.Z) and commit
 	@NEW_VERSION=$$(echo $(VERSION) | awk -F. '{print $$1"."$$2"."$$3+1}') && \
-	sed -i "s/VERSION=$(VERSION)/VERSION=$$NEW_VERSION/" Makefile && \
+	$(SED_I) "s/VERSION=$(VERSION)/VERSION=$$NEW_VERSION/" Makefile && \
 	[ -f $(PACKAGE_DIR)/__init__.py ] && grep -q '__version__' $(PACKAGE_DIR)/__init__.py && \
-		sed -i "s/__version__ = \"$(VERSION)\"/__version__ = \"$$NEW_VERSION\"/" $(PACKAGE_DIR)/__init__.py && \
+		$(SED_I) "s/__version__ = \"$(VERSION)\"/__version__ = \"$$NEW_VERSION\"/" $(PACKAGE_DIR)/__init__.py && \
 		git add $(PACKAGE_DIR)/__init__.py || true && \
 	git add Makefile && \
 	git commit -m "[CC] chore: bump version to $$NEW_VERSION" && \
@@ -19,9 +22,9 @@ bump-patch: ## bump patch version (x.y.Z) and commit
 
 bump-minor: ## bump minor version (x.Y.0) and commit
 	@NEW_VERSION=$$(echo $(VERSION) | awk -F. '{print $$1"."$$2+1".0"}') && \
-	sed -i "s/VERSION=$(VERSION)/VERSION=$$NEW_VERSION/" Makefile && \
+	$(SED_I) "s/VERSION=$(VERSION)/VERSION=$$NEW_VERSION/" Makefile && \
 	[ -f $(PACKAGE_DIR)/__init__.py ] && grep -q '__version__' $(PACKAGE_DIR)/__init__.py && \
-		sed -i "s/__version__ = \"$(VERSION)\"/__version__ = \"$$NEW_VERSION\"/" $(PACKAGE_DIR)/__init__.py && \
+		$(SED_I) "s/__version__ = \"$(VERSION)\"/__version__ = \"$$NEW_VERSION\"/" $(PACKAGE_DIR)/__init__.py && \
 		git add $(PACKAGE_DIR)/__init__.py || true && \
 	git add Makefile && \
 	git commit -m "[CC] chore: bump version to $$NEW_VERSION" && \
@@ -29,9 +32,9 @@ bump-minor: ## bump minor version (x.Y.0) and commit
 
 bump-major: ## bump major version (X.0.0) and commit
 	@NEW_VERSION=$$(echo $(VERSION) | awk -F. '{print $$1+1".0.0"}') && \
-	sed -i "s/VERSION=$(VERSION)/VERSION=$$NEW_VERSION/" Makefile && \
+	$(SED_I) "s/VERSION=$(VERSION)/VERSION=$$NEW_VERSION/" Makefile && \
 	[ -f $(PACKAGE_DIR)/__init__.py ] && grep -q '__version__' $(PACKAGE_DIR)/__init__.py && \
-		sed -i "s/__version__ = \"$(VERSION)\"/__version__ = \"$$NEW_VERSION\"/" $(PACKAGE_DIR)/__init__.py && \
+		$(SED_I) "s/__version__ = \"$(VERSION)\"/__version__ = \"$$NEW_VERSION\"/" $(PACKAGE_DIR)/__init__.py && \
 		git add $(PACKAGE_DIR)/__init__.py || true && \
 	git add Makefile && \
 	git commit -m "[CC] chore: bump version to $$NEW_VERSION" && \
